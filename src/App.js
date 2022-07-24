264134e7aa22488f0fc4f2cee29143d1ca7c6795
GrahamP98 <g.purnell15@gmail.com>
2022-07-24 18:26:15 -0400

    set up react and file stuctures

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
